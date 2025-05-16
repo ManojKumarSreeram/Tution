e58ced23d464882d1d493c8c1a7d10991419ce2f
ManojKumarSreeram <sreerammanoj777@gmail.com>
2025-05-16 07:32:54 +0530

    implemented update student logic

---
+++ Service/update_student_details_service.py
@@ -0,0 +1,68 @@
+
+import bcrypt
+from Repository.db_operations import inser_data
+from Utilities.custom_exceptions import BadRequestException,CustomAPIException
+import logging
+logging.basicConfig(level=logging.INFO)
+from datetime import datetime
+
+# Function to hash a plain password
+def hash_password(plain_password: str) -> bytes:
+    # Generate salt and hash password
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(plain_password.encode('utf-8'), salt)
+    return hashed
+
+def process_student_updated_details(params):
+    try:
+        logging.info("start of process_student_updated_details function")
+        teacher_id = params.get("teacher_id").strip()
+        first_name=params.get("first_name").strip()
+        last_name=params.get("last_name").strip()
+        email=params.get("email").strip()
+        password=params.get("password").strip()
+        phone_number=params.get("phone_number").strip()
+        gender=params.get("gender").strip()
+        student_id = params.get("student_id").strip()
+
+        query = """
+                    UPDATE student_login
+                    SET teacher_id = %s,
+                        first_name = %s,
+                        last_name = %s,
+                        email = %s,
+                        password = %s,
+                        phone_number = %s,
+                        gender = %s,
+                        modified_at=%s
+                    WHERE student_id = %s;
+                """
+
+        # Get current datetime
+        now = datetime.now()
+
+        # Print it in the format: YYYY-MM-DD HH:MM:SS.microseconds
+        formatted_date = now.strftime("%Y-%m-%d %H:%M:%S.%f")
+        print(formatted_date)
+        hashed_password=hash_password(password)
+        values = (teacher_id,first_name,last_name,email,hashed_password,phone_number,gender,formatted_date,student_id)
+        result=inser_data(query,values)
+        return {"data":"data is Updated successfully","status_code":200}
+    except CustomAPIException as ce:
+        logging.info("customexception in process_student_updated_details function")
+        query = """
+                INSERT INTO error_logs (error,file_name)
+                VALUES (%s, %s);
+            """
+        values = (str(ce),__name__)
+        inser_data(query,values)
+        raise ce
+    except Exception as e:
+        logging.info("unknown in process_student_updated_details function")
+        query = """
+                INSERT INTO error_logs (error,file_name)
+                VALUES (%s, %s);
+            """
+        values = (str(e),__name__)
+        inser_data(query,values)
+        raise BadRequestException(str(e))
